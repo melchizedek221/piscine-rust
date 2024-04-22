8cf0a99fe1c8249853fd2a9e1e583d4e85356169
Djibril SOW <djibrilsow1@esp.sn>
2024-04-22 14:00:14 +0000

    at_indx

---
+++ groceries/src/lib.rs
@@ -0,0 +1,22 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+
+pub fn at_index(vec: &Vec<String>, index: usize) -> String {
+    vec[index].clone() 
+}
