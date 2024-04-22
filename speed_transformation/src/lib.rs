224621a992c10e79ef074b0f0f20195e6312116f
Djibril SOW <djibrilsow1@esp.sn>
2024-04-22 12:35:23 +0000

    second sub

--- speed_transformation/src/lib.rs
+++ speed_transformation/src/lib.rs
@@ -1,17 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
 
 pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+    km_h * 1000.0 / 3600.0
 }
