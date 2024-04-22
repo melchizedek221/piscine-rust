bdd853850529a3885eb3d846d761996822d3f777
Djibril SOW <djibrilsow1@esp.sn>
2024-04-22 12:34:53 +0000

    km/h to m/s

---
+++ speed_transformation/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+}
