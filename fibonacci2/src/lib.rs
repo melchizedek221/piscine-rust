614980770558d4fff83dbcd7cd4ae83b079658e5
Djibril SOW <djibrilsow1@esp.sn>
2024-04-22 15:49:46 +0000

    fibo

---
+++ fibonacci2/src/lib.rs
@@ -0,0 +1,34 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+pub fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    }
+
+    let mut a = 0;
+    let mut b = 1;
+    let mut result = 0;
+
+    for _ in 2..=n {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+
+    return result;
+}
