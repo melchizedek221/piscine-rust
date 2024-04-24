39da06c55fc44eff78f30d2555f0383b4229e9a4
Djibril SOW <djibrilsow1@esp.sn>
2024-04-24 11:31:39 +0000

    tic tac bouum

---
+++ tic_tac_toe/src/lib.rs
@@ -0,0 +1,42 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+
+pub fn tic_tac_toe(table: Vec<Vec<&str>>) -> String {
+    if check_winner(&'O'.to_string(), &table) {
+        "Player O won".to_string()
+    } else if check_winner(&'X'.to_string(), &table) {
+        "Player X won".to_string()
+    } else {
+        "Tie".to_string()
+    }
+}
+
+pub fn check_winner(player: &str, table: &Vec<Vec<&str>>) -> bool {
+    diagonals(player, table) || horizontal(player, table) || vertical(player, table)
+}
+
+pub fn diagonals(player: &str, table: &Vec<Vec<&str>>) -> bool {
+    (0..table.len()).all(|i| table[i][i] == player) ||
+    (0..table.len()).all(|i| table[i][table.len() - 1 - i] == player)
+}
+
+pub fn horizontal(player: &str, table: &Vec<Vec<&str>>) -> bool {
+    table.iter().any(|row| row.iter().all(|&cell| cell == player))
+}
+
+pub fn vertical(player: &str, table: &Vec<Vec<&str>>) -> bool {
+    (0..table[0].len()).any(|col| (0..table.len()).all(|row| table[row][col] == player))
+}
