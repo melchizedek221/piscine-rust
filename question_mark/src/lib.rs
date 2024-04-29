5b724f195f7727aaeeb5a1a36ac98fc7a96dbec3
Djibril SOW <djibrilsow1@esp.sn>
2024-04-29 14:47:37 +0000

    test

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -13,10 +13,10 @@
 //     }
 // }
 
-pub struct One { first_layer: Option<Two> }
-pub struct Two { second_layer: Option<Three> }
-pub struct Three { third_layer: Option<Four> }
-pub struct Four { fourth_layer: Option<u16> }
+pub struct One { pub first_layer: Option<Two> }
+pub struct Two { pub second_layer: Option<Three> }
+pub struct Three { pub third_layer: Option<Four> }
+pub struct Four { pub fourth_layer: Option<u16> }
 
 // impl One {
 //     pub fn get_fourth_layer(&self) -> Option<u16> {
