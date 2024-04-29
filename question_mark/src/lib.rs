9502377de636a4e0f8da09f59854c401446c2192
Djibril SOW <djibrilsow1@esp.sn>
2024-04-29 14:46:44 +0000

    test

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -18,6 +18,21 @@ pub struct Two { second_layer: Option<Three> }
 pub struct Three { third_layer: Option<Four> }
 pub struct Four { fourth_layer: Option<u16> }
 
+// impl One {
+//     pub fn get_fourth_layer(&self) -> Option<u16> {
+//         match &self.first_layer {
+//             Some(two) => match &two.second_layer {
+//                 Some(three) => match &three.third_layer {
+//                     Some(four) => four.fourth_layer,
+//                     None => None,
+//                 },
+//                 None => None,
+//             },
+//             None => None,
+//         }
+//     }
+// }
+
 impl One {
     pub fn get_fourth_layer(&self) -> Option<u16> {
         self.first_layer.as_ref()?.second_layer.as_ref()?.third_layer.as_ref()?.fourth_layer
