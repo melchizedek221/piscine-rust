7544bbe939504d68c76dd13a39a406a59b55f9bd
Djibril SOW <djibrilsow1@esp.sn>
2024-04-29 14:11:26 +0000

    test

---
+++ profanity_filter/src/lib.rs
@@ -0,0 +1,42 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+#[derive(Debug)]
+pub struct Message {
+    content:  String,
+    user: String
+}
+
+#[derive(Debug)]
+impl Message {
+    pub fn new(ms: String, u: String) -> Message {
+        Message { content:ms , user:u }
+    }
+
+    pub fn send_ms(&self) -> Option<&str> {
+        if self.content.is_empty() || self.content.contains("stupid") {
+            None
+        }else{
+            Some(self.content)
+        }
+    }
+}
+
+pub fn check_ms(ms: &Message) -> (bool, &str) {
+    match ms.send_ms() {
+        Some(content) => (true, content),
+        None => (false, "ERROR: illegal")
+    }
+}
