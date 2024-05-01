2f2a6b8fb466d4fe24acd3eddc7c8bad90217fff
Djibril SOW <djibrilsow1@esp.sn>
2024-05-01 12:55:48 +0000

    bim force

---
+++ scores/src/lib.rs
@@ -0,0 +1,37 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+pub fn score(s: &str) -> u64 {
+    let mut score = 0;
+    for c in s.chars() {
+        match c.to_ascii_lowercase() {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => score += 1,
+            'd' | 'g' => score += 2,
+            'b' | 'c' | 'm' | 'p' => score += 3,
+            'f' | 'h' | 'v' | 'w' | 'y' => score += 4,
+            'k' => score += 5,
+            'j' | 'x' => score += 8,
+            'q' | 'z' => score += 10,
+            _ => (),
+        }
+    }
+    score
+}
+
+// fn main() {
+    println!("{}", score("a"));
+    println!("{}", score("ã ê Á?"));
+    println!("{}", score("ThiS is A Test"));
+}
