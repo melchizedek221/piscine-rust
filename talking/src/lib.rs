bfd3b66d9614c1e901f960b09ac6e937a7d3329b
Djibril SOW <djibrilsow1@esp.sn>
2024-05-01 14:03:26 +0000

    test

---
+++ talking/src/lib.rs
@@ -0,0 +1,28 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+pub fn talking(text: &str) -> &str {
+    if text.is_empty() {
+        "Just say something!"
+    } else if text.is_uppercase() {
+        if text.contains('?') {
+            "Quiet, I am thinking!"
+        } else {
+            "There is no need to yell, calm down!"
+        }
+    } else {
+        "Sure."
+    }
+}
