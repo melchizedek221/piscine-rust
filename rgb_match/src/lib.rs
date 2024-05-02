63a3265f4d78a78e3cdd03952ce33bd35793833b
Djibril SOW <djibrilsow1@esp.sn>
2024-05-02 00:16:40 +0000

    test

--- rgb_match/src/lib.rs
+++ rgb_match/src/lib.rs
@@ -24,18 +24,18 @@ pub struct Color {
 impl Color {
     pub fn swap(mut self, first: u8, second: u8) -> Color {
         match (self.r, self.g, self.b, self.a) {
-            (r, g, _, _) if r == first && g == second => { self.r = second; self.g = first; }
-            (r, _, b, _) if r == first && b == second => { self.r = second; self.b = first; }
-            (r, _, _, a) if r == first && a == second => { self.r = second; self.a = first; }
-            (_, g, r, _) if g == first && r == second => { self.g = second; self.r = first; }
-            (_, g, _, b) if g == first && b == second => { self.g = second; self.b = first; }
-            (_, g, _, a) if g == first && a == second => { self.g = second; self.a = first; }
-            (_, _, b, r) if b == first && r == second => { self.b = second; self.r = first; }
-            (_, _, b, g) if b == first && g == second => { self.b = second; self.g = first; }
-            (_, _, b, a) if b == first && a == second => { self.b = second; self.a = first; }
-            (_, _, _, r) if self.a == first && r == second => { self.a = second; self.r = first; }
-            (_, _, _, g) if self.a == first && g == second => { self.a = second; self.g = first; }
-            (_, _, _, b) if self.a == first && b == second => { self.a = second; self.b = first; }
+            (r, g, _, _) if self.r == first && self.g == second => { self.r = second; self.g = first; }
+            (r, _, b, _) if self.r == first && self.b == second => { self.r = second; self.b = first; }
+            (r, _, _, a) if self.r == first && self.a == second => { self.r = second; self.a = first; }
+            (_, g, r, _) if self.g == first && self.r == second => { self.g = second; self.r = first; }
+            (_, g, _, b) if self.g == first && self.b == second => { self.g = second; self.b = first; }
+            (_, g, _, a) if self.g == first && self.a == second => { self.g = second; self.a = first; }
+            (_, _, b, r) if self.b == first && self.r == second => { self.b = second; self.r = first; }
+            (_, _, b, g) if self.b == first && self.g == second => { self.b = second; self.g = first; }
+            (_, _, b, a) if self.b == first && self.a == second => { self.b = second; self.a = first; }
+            (_, _, _, r) if self.a == first && self.r == second => { self.a = second; self.r = first; }
+            (_, _, _, g) if self.a == first && self.g == second => { self.a = second; self.g = first; }
+            (_, _, _, b) if self.a == first && self.b == second => { self.a = second; self.b = first; }
             _ => {}
         }
         self
