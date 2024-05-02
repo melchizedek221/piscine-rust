cc1dc7de85f3b1543555df60de67ea9496e1a700
Djibril SOW <djibrilsow1@esp.sn>
2024-05-02 04:13:15 +0000

    last commit

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -13,23 +13,60 @@
 //     }
 // }
 
-const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
-
 pub fn pig_latin(text: &str) -> String {
-    text.split_whitespace()
-        .map(|word| {
-            let mut new = word.trim_start_matches(|ch| !VOWELS.contains(&ch));
-            let mut removed = word.len() - new.len();
-            if &word[removed.saturating_sub(1)..=removed] == "qu" {
-                removed += 1;
-                new = &word[removed..];
+    fn is_vowel(c: char) -> bool {
+        matches!(c, 'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U')
+    }
+    
+    let mut new_string = text.to_string();
+    let mut next_char = false;
+
+    for (i, c) in text.chars().enumerate() {
+        if next_char && (c == 'q' || c == 'u') {
+            new_string.remove(0);
+            println!("{}", new_string);
+            new_string.push(c);
+            if c == 'u' {
+                next_char = false;
             }
-            let consonants = &word[0..removed];
-            String::with_capacity(word.len() + 2) + new + consonants + "ay"
-        })
-        .collect::<Vec<_>>()
-        .join(" ")
+        } else if !is_vowel(c) && text.len() > i + 2 && &text[i + 1..i + 3] == "qu" {
+            new_string.remove(0);
+            new_string.push(c);
+            next_char = true;
+        } else if i == 0 && is_vowel(c) {
+            break;
+        } else if !is_vowel(c) {
+            new_string.remove(0);
+            new_string.push(c);
+        } else {
+            break;
+        }
+    }
+    new_string + "ay"
 }
+
+
+
+// fn main() {
+    // println!("{}", pig_latin(&String::from("igloo")));
+    // println!("{}", pig_latin(&String::from("apple")));
+    // println!("{}", pig_latin(&String::from("hello")));
+    // println!("{}", pig_latin(&String::from("square")));
+    // println!("{}", pig_latin(&String::from("xenon")));
+    // println!("{}", pig_latin(&String::from("chair")));
+	// println!("{}", pig_latin(&String::from("queen")));
+// }
+// And its output:
+
+// $ cargo run
+// iglooay
+// appleay
+// ellohay
+// aresquay
+// enonxay
+// airchay
+// eenquay
+// $
 
 // fn main() {
 //     println!("{}", pig_latin(&String::from("igloo")));
