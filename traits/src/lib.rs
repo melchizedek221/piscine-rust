79061487cce38eecc371bd70d4e996f61dd0f186
Djibril SOW <djibrilsow1@esp.sn>
2024-05-09 12:30:34 +0000

    test

---
+++ traits/src/lib.rs
@@ -0,0 +1,52 @@
+#[derive(Debug)]
+pub struct Player {
+	pub name: String,
+	pub strength: f64,
+	pub score: i32,
+	pub money: i32,
+	pub weapons: Vec<String>,
+}
+
+impl std::fmt::Display for Player {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} \nStrength: {}, Score: {}, Money: {} \nWeapons: {:?}",
+        self.name,
+        self.strength,
+        self.score,
+        self.money,
+        self.weapons)
+    }
+}
+
+pub struct Fruit {
+	pub weight_in_kg: f64,
+}
+
+pub struct Meat {
+	pub weight_in_kg: f64,
+	pub fat_content: f64,
+}
+
+impl Player {
+	fn eat<T: Food>(&mut self, food: T) {
+		self.strength += food.gives();
+	}
+}
+
+pub trait Food {
+	fn gives(&self) -> f64;
+}
+
+impl Food for Fruit {
+	fn gives(&self) -> f64{
+	    self.weight_in_kg * 4.0
+	}
+}
+
+impl Food for Meat {
+    fn gives(&self) -> f64 {
+        let protein = (1.0 - self.fat_content) * self.weight_in_kg * 4.0;
+        let fat = self.fat_content * self.weight_in_kg * 9.0;
+        protein + fat
+    }
+}
