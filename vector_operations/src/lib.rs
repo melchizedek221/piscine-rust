b5ca45f2f241d3f7eaea8ad8e3c344cde8d75801
Djibril SOW <djibrilsow1@esp.sn>
2024-05-09 15:27:59 +0000

    test

---
+++ vector_operations/src/lib.rs
@@ -0,0 +1,48 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct ThreeDVector<T> {
+    pub i: T,
+    pub j: T,
+    pub k: T,
+}
+
+use std::ops::{Add, Sub};
+
+impl<T: Add<Output = T>> Add for ThreeDVector<T> {
+    type Output = ThreeDVector<T>;
+
+    fn add(self, other: ThreeDVector<T>) -> ThreeDVector<T> {
+        ThreeDVector {
+            i: self.i + other.i,
+            j: self.j + other.j,
+            k: self.k + other.k,
+        }
+    }
+}
+
+impl<T: Sub<Output = T>> Sub for ThreeDVector<T> {
+    type Output = ThreeDVector<T>;
+
+    fn sub(self, other: ThreeDVector<T>) -> ThreeDVector<T> {
+        ThreeDVector {
+            i: self.i - other.i,
+            j: self.j - other.j,
+            k: self.k - other.k,
+        }
+    }
+}
+
