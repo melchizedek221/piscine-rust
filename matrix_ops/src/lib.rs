5d0b423ac230f6c40bda88227d61a45ba0e8ffad
Djibril SOW <djibrilsow1@esp.sn>
2024-05-14 22:10:57 +0000

    changes

--- matrix_ops/src/lib.rs
+++ matrix_ops/src/lib.rs
@@ -1,120 +1,114 @@
-use std::ops::{ Add, Sub };
-use lalgebra_scalar::Scalar;
+use std::ops::{Add, Sub};
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+pub trait Scalar: Sized + std::ops::Add<Self, Output = Self> + std::ops::Sub<Self, Output = Self> + std::ops::Mul<Self, Output = Self> + std::ops::Div<Self, Output = Self> {
+    type Item;
+    fn zero() -> Self::Item;
+    fn one() -> Self::Item;
+}
+
+impl Scalar for u32 {
+    type Item = u32;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for u64 {
+    type Item = u64;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for i32 {
+    type Item = i32;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for i64 {
+    type Item = i64;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for f32 {
+    type Item = f32;
+    fn zero() -> Self::Item {
+        0.
+    }
+    fn one() -> Self::Item {
+        1.
+    }
+}
+
+impl Scalar for f64 {
+    type Item = f64;
+    fn zero() -> Self::Item {
+        0.
+    }
+    fn one() -> Self::Item {
+        1.
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct Matrix<T>(pub Vec<Vec<T>>);
-impl <T: Scalar<Item = T> + std::clone::Clone> Matrix<T> {
-	pub fn new() -> Matrix<T> {
-        return Matrix(vec![]);
-	}
-	pub fn zero(row: usize, col: usize) -> Matrix<T> {
-        return Matrix(vec![vec![T::zero(); col]; row]);
-	}
-	pub fn identity(n: usize) -> Matrix<T> {
-        let mut result = Matrix::zero(n, n);
-        for i in 0..n{
-            result.0[i][i] = T::one();
+
+impl<T: Add<Output = T> + Sub<Output = T> + Clone> Add for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+
+    fn add(self, other: Matrix<T>) -> Self::Output {
+        let rows = self.0.len();
+        let cols = self.0[0].len();
+        if other.0.len() != rows || other.0[0].len() != cols {
+            return None;
         }
-        return result;
-	}#[derive(Debug, PartialEq)]
-    pub struct Matrix(pub Vec<Vec<i32>>);
-    
-    
-    use std::ops::{Add, Sub};
-    
-    impl Add for Matrix {
-        type Output = Option<Matrix>;
-    
-        fn add(self, other: Matrix) -> Self::Output {
-            if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
-                return None;
+
+        let mut result = Vec::with_capacity(rows);
+        for i in 0..rows {
+            let mut row = Vec::with_capacity(cols);
+            for j in 0..cols {
+                row.push(self.0[i][j].clone() + other.0[i][j].clone());
             }
-    
-            let result = self.0.iter().zip(other.0.iter())
-                .map(|(row_a, row_b)| {
-                    row_a.iter().zip(row_b.iter())
-                        .map(|(&x, &y)| x + y)
-                        .collect()
-                })
-                .collect();
-    
-            Some(Matrix(result))
+            result.push(row);
         }
-    }
-    
-    impl Sub for Matrix {
-        type Output = Option<Matrix>;
-    
-        fn sub(self, other: Matrix) -> Self::Output {
-            if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
-                return None;
-            }
-    
-            let result = self.0.iter().zip(other.0.iter())
-                .map(|(row_a, row_b)| {
-                    row_a.iter().zip(row_b.iter())
-                        .map(|(&x, &y)| x - y)
-                        .collect()
-                })
-                .collect();
-    
-            Some(Matrix(result))
-        }
+        Some(Matrix(result))
     }
 }
 
-impl <T> Add for Matrix<T> 
-where
-    T: Scalar + Scalar<Item = T> + Clone + Add<Output = T> + std::ops::AddAssign,
-{
+impl<T: Add<Output = T> + Sub<Output = T> + Clone> Sub for Matrix<T> {
     type Output = Option<Matrix<T>>;
-    fn add(self, other: Self) -> Self::Output {
-        let self_row_len = self.0.len();
-        let self_col_len = self.0[0].len();
-        let other_row_len = other.0.len();
 
-        if self_row_len != other_row_len{
-            return  None;
+    fn sub(self, other: Matrix<T>) -> Self::Output {
+        let rows = self.0.len();
+        let cols = self.0[0].len();
+        if other.0.len() != rows || other.0[0].len() != cols {
+            return None;
         }
-        for row_ind in 0..self_row_len{
-            if self.0[row_ind].len() != other.0[row_ind].len(){
-                return None;
-            }
-        }
-        
-        let mut result = Matrix::<T>::zero(self_row_len, self_col_len);
-        for row in 0..self_row_len{
-            for col in 0..self_col_len{
-                result.0[row][col] = self.0[row][col].clone() + other.0[row][col].clone(); 
-            }
-        }
-        return Some(result);
 
-    }
-}
-
-impl <T:Scalar<Item = T>  + Clone + Sub<Output = T>> Sub for Matrix<T> {
-    type Output = Option<Matrix<T>>;
-    fn sub(self, other: Self) -> Self::Output {
-        let self_row_len = self.0.len();
-        let self_col_len = self.0[0].len();
-        let other_row_len = other.0.len();
-        
-        if self_row_len != other_row_len{
-            return  None;
-        }
-        for row_ind in 0..self_row_len{
-            if self.0[row_ind].len() != other.0[row_ind].len(){
-                return None;
+        let mut result = Vec::with_capacity(rows);
+        for i in 0..rows {
+            let mut row = Vec::with_capacity(cols);
+            for j in 0..cols {
+                row.push(self.0[i][j].clone() - other.0[i][j].clone());
             }
+            result.push(row);
         }
-        
-        let mut result = Matrix::<T>::zero(self_row_len, self_col_len);
-        for row in 0..self_row_len{
-            for col in 0..self_col_len{
-                result.0[row][col] = self.0[row][col].clone() - other.0[row][col].clone(); 
-            }
-        }
-        return Some(result);
+        Some(Matrix(result))
     }
 }
