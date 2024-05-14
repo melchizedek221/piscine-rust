e85dad99d91a062cf6fdb4a205a089836a1508f6
Djibril SOW <djibrilsow1@esp.sn>
2024-05-14 22:02:37 +0000

    changes

--- matrix_ops/src/ops.rs
+++ matrix_ops/src/ops.rs
@@ -16,7 +16,51 @@ impl <T: Scalar<Item = T> + std::clone::Clone> Matrix<T> {
             result.0[i][i] = T::one();
         }
         return result;
-	}
+	}#[derive(Debug, PartialEq)]
+    pub struct Matrix(pub Vec<Vec<i32>>);
+    
+    
+    use std::ops::{Add, Sub};
+    
+    impl Add for Matrix {
+        type Output = Option<Matrix>;
+    
+        fn add(self, other: Matrix) -> Self::Output {
+            if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+                return None;
+            }
+    
+            let result = self.0.iter().zip(other.0.iter())
+                .map(|(row_a, row_b)| {
+                    row_a.iter().zip(row_b.iter())
+                        .map(|(&x, &y)| x + y)
+                        .collect()
+                })
+                .collect();
+    
+            Some(Matrix(result))
+        }
+    }
+    
+    impl Sub for Matrix {
+        type Output = Option<Matrix>;
+    
+        fn sub(self, other: Matrix) -> Self::Output {
+            if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+                return None;
+            }
+    
+            let result = self.0.iter().zip(other.0.iter())
+                .map(|(row_a, row_b)| {
+                    row_a.iter().zip(row_b.iter())
+                        .map(|(&x, &y)| x - y)
+                        .collect()
+                })
+                .collect();
+    
+            Some(Matrix(result))
+        }
+    }
 }
 
 impl <T> Add for Matrix<T> 
