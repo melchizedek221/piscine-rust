8ceb5b5da913a8b70ade6ea44b1c424585202a79
Djibril SOW <djibrilsow1@esp.sn>
2024-05-14 22:13:56 +0000

    bim

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,29 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+#[derive(Debug)]
+pub struct Person<'a> {
+    pub name: &'a str,
+    pub age: u8,
+}
+
+impl<'a> Person<'a> {
+    pub fn new(name: &'a str) -> Person<'a> {
+        Person {
+            name: name,
+            age: 0,
+        }
+    }
+}
