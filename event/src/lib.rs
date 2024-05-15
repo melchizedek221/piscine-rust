15f81062d875d623f9cf54d2876355ff203ac66f
Djibril SOW <djibrilsow1@esp.sn>
2024-05-15 17:58:02 +0000

    bim

--- event/src/lib.rs
+++ event/src/lib.rs
@@ -1,28 +1,10 @@
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
 use chrono::Duration;
-use colored::*;
-use std::fmt;
-
-#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum Position {
     Top,
     Bottom,
     Center,
 }
-
 #[derive(Debug, Eq, PartialEq)]
 pub struct Notification {
     pub size: u32,
@@ -30,7 +12,6 @@ pub struct Notification {
     pub position: Position,
     pub content: String,
 }
-
 #[derive(Debug)]
 pub enum Event<'a> {
     Remainder(&'a str),
@@ -38,50 +19,36 @@ pub enum Event<'a> {
     Appointment(&'a str),
     Holiday,
 }
-
-impl<'a> fmt::Display for Notification {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "({}, {}, {})",
-            match self.position {
-                Position::Top => "Top",
-                Position::Bottom => "Bottom",
-                Position::Center => "Center",
-            },
-            self.size,
-            self.content.truecolor(self.color.0, self.color.1, self.color.2)
-        )
+use std::fmt;
+use std::fmt::{Formatter};
+impl fmt::Display for Notification {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "({:?}, {:?}, \x1b[38;2;{};{};{}m{}\x1b[0m)", self.position, self.size, self.color.0, self.color.1, self.color.2, self.content)
     }
 }
-
+pub use Event::*;
 impl<'a> Event<'a> {
     pub fn notify(&self) -> Notification {
         match self {
-            Event::Remainder(text) => Notification {
+            Remainder(s) => Notification{
                 size: 50,
                 color: (50, 50, 50),
                 position: Position::Bottom,
-                content: text.to_string(),
+                content: s.to_string(),
             },
-            Event::Registration(duration) => Notification {
+            Registration(d) => Notification{
                 size: 30,
                 color: (255, 2, 22),
                 position: Position::Top,
-                content: format!(
-                    "You have {}H:{}M:{}S left before the registration ends",
-                    duration.num_hours(),
-                    duration.num_minutes() % 60,
-                    duration.num_seconds() % 60
-                ),
+                content: format!("You have {}H:{}M:{}S left before the registration ends", d.num_hours(), d.num_minutes() % 60, d.num_seconds() % 60),
             },
-            Event::Appointment(text) => Notification {
+            Appointment(s) => Notification{
                 size: 100,
                 color: (200, 200, 3),
                 position: Position::Center,
-                content: text.to_string(),
+                content: s.to_string(),
             },
-            Event::Holiday => Notification {
+            Holiday => Notification{
                 size: 25,
                 color: (0, 255, 0),
                 position: Position::Top,
