d9095857502bc491c04b8491c09c231eb57ef409
Djibril SOW <djibrilsow1@esp.sn>
2024-05-17 19:22:40 +0000

    yo

---
+++ adding/src/lib.rs
@@ -0,0 +1,18 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+fn add_curry(x: i32) -> Box<dyn Fn(i32) -> i32> {
+    Box::new(move |y| x + y)
+}
