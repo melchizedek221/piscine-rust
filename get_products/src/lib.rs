501a4c0d84544d7178b1f9e452ae0f41ddeeab9a
Djibril SOW <djibrilsow1@esp.sn>
2024-05-17 19:31:33 +0000

    bim bim bim

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,19 +1,9 @@
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
-
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len() == 1 {
+        return vec![1];
+    }
+    
     let product: usize = arr.iter().product();
-    arr.into_iter().map(|n| product / n).collect()
+    
+    arr.into_iter().map(|n| product / n).collect::<Vec<usize>>()
 }
