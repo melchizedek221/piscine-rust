599f057256d3817b0b914cb0c74970e7ccbabe53
Djibril SOW <djibrilsow1@esp.sn>
2024-05-17 20:58:46 +0000

    changes

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,47 +1,44 @@
-// Structure Collatz
 #[derive(Copy, Clone)]
 pub struct Collatz {
     pub v: u64,
 }
 
+impl Iterator for Collatz {
+    type Item = Collatz;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.v == 0 || self.v == 1{
+            return None;
+        }else if self.v % 2 == 0{
+            self.v /= 2;
+            Some(Collatz::new(self.v * 2))
+        }else {
+            self.v = self.v * 3 + 1;
+            Some(Collatz::new((self.v - 1) / 3))
+        }
+    }
+}
+
 impl Collatz {
-    // Constructeur pour Collatz
-    pub fn new(n: u64) -> Self {
+	pub fn new(n: u64) -> Self {
         Collatz { v: n }
     }
-
-    // Renvoie la prochaine valeur dans la séquence de Collatz
-    fn next_val(&mut self) {
-        if self.v % 2 == 0 {
-            self.v /= 2;
-        } else {
-            self.v = self.v * 3 + 1;
-        }
-    }
 }
 
-// Implémentation de l'itérateur pour Collatz
-impl Iterator for Collatz {
-    type Item = Self;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.v == 1 || self.v == 0 {
-            None
-        } else {
-            self.next_val();
-            Some(*self)
-        }
-    }
-}
-
-// Fonction qui compte le nombre de pas nécessaires pour atteindre 1
 pub fn collatz(n: u64) -> usize {
-    let mut collatz_iter = Collatz::new(n);
-    let mut counter = 0;
-
-    while let Some(_) = collatz_iter.next() {
-        counter += 1;
+    if n == 0{
+        return 0;
     }
 
-    counter
+    let mut counter = 0;
+    let mut collatz_strt = Collatz::new(n);
+    while collatz_strt.v != 1 {
+        counter += 1;
+        if collatz_strt.v % 2 == 0{
+            collatz_strt.v = collatz_strt.v / 2;
+        }else {
+            collatz_strt.v = collatz_strt.v * 3 + 1;
+        }
+    }
+
+    return counter;
 }
